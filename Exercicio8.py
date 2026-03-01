9b0103f9b08c4549bd92d105e4d6d1a1c9a465d5
Igor Gomes <igor.barbosa.tads@gmail.com>
2026-02-28 21:21:52 -0300

    Update Exercicio8.py

--- Exercicio8.py
+++ Exercicio8.py
@@ -1,17 +1,16 @@
-print("=== Contador de Numeros Pares ===")
+print("=== Contador de Números Pares ===")
 
 contador = int(input("Digite um valor: "))
-if contador != 0:
-   print(f"Nota:{contador} é um Número Ímpar. Começando do Número Anterior...")
 
-   contador-1
-   
-while 0 < contador:
-   contador -= 2
+if contador % 2 != 0:
+    print(f"Nota: {contador} é um Número Ímpar. Começando do número par anterior...")
+    contador -= 1
 
-   print(f"{contador} é um Número Par")
-   
-   if contador == 0:
-      print("Fim da Contagem!!")
-      break
-   
+while contador >= 0:
+    print(f"{contador} é um Número Par")
+    
+    if contador == 0:
+        print("Fim da Contagem!!")
+        break
+    
+    contador -= 2
