9eba3f2ea55a8e913a7419dd13364992e120a9fb
Igor Gomes <igor.barbosa.tads@gmail.com>
2026-02-28 21:25:44 -0300

    Delete Exercicio3.py

--- Exercicio3.py
+++
@@ -1,9 +0,0 @@
-print("=== Verificaçäo de Idade Para Dirigir ===")
-
-idade = float(input("Digite a Sua Idade: "))
-
-if idade <18:
-    print("Näo Possui Idade Para Dirigir!!!!")
-
-elif idade >=18:
-    print("Pode Tirar Carteira de Motorista!!! ")
